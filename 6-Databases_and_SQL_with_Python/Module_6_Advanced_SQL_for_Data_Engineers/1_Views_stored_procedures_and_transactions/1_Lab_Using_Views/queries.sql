60f18f96848cfbece68110cec7e2ec58a4d9a7e3
Marcus Faustino <marcus.faustino1@gmail.com>
2025-07-29 08:28:29 -0300

    Completed Module 6

---
+++ 6-Databases_and_SQL_with_Python/Module_6_Advanced_SQL_for_Data_Engineers/1_Views_stored_procedures_and_transactions/1_Lab_Using_Views/queries.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE VIEW EMPSALARY AS
+SELECT EMP_ID, F_NAME, L_NAME, B_DATE, SEX, JOB_TITLE,
+MIN_SALARY, MAX_SALARY
+FROM EMPLOYEES, JOBS
+WHERE EMPLOYEES.JOB_ID = JOBS.JOB_IDENT;
+
+SELECT * FROM EMPSALARY;
+
+DROP VIEW EMPSALARY;
+
+SELECT * FROM EMPSALARY;
+
+-- Practice Problems
+
+-- Create a view “EMP_DEPT” which has the following information.
+CREATE OR REPLACE VIEW EMP_DEPT AS
+SELECT EMP_ID, F_NAME, L_NAME, DEP_ID from EMPLOYEES;
+
+SELECT * FROM EMP_DEPT;
+
+-- Modify “EMP_DEPT” such that it displays Department names instead of Department IDs. 
+-- For this, we need to combine information from EMPLOYEES and DEPARTMENTS as follows.
+
+-- EMP_ID, FNAME, LNAME from EMPLOYEES table and
+-- DEP_NAME from DEPARTMENTS table, combined over the columns DEP_ID and DEPT_ID_DEP.
+
+CREATE OR REPLACE VIEW EMP_DEPT AS
+SELECT EMP_ID, F_NAME, L_NAME, DEP_NAME from EMPLOYEES E, DEPARTMENTS D WHERE E.DEP_ID = D.DEPT_ID_DEP;
+
+SELECT * FROM EMP_DEPT;
+
+-- Drop the view “EPM_DEPT”.
+DROP VIEW EMP_SALARY;
